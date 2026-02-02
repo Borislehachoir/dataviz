12da8fc9eb1d7e97b68ae6b6f9aded232d4fffd2
Borislehachoir <borisrocc@gmail.com>
2026-02-02 16:30:46 +0100

    modification map et revue de l'index

--- carte.js
+++ carte.js
@@ -4,59 +4,59 @@
 let map, geoLayer;
 
 const countryNamesFr = {
-    "Albania":"Albanie","Algeria":"Algérie","Andorra":"Andorre",
-    "Antigua and Barbuda":"Antigua-et-Barbuda","Argentina":"Argentine",
-    "Armenia":"Arménie","Australia":"Australie","Austria":"Autriche",
-    "Azerbaijan":"Azerbaïdjan","Bahrain":"Bahreïn","Barbados":"Barbade",
-    "Belarus":"Biélorussie","Belgium":"Belgique","Benin":"Bénin",
-    "Bhutan":"Bhoutan","Bolivia":"Bolivie","Bosnia and Herz.":"Bosnie-Herzégovine",
-    "Brazil":"Brésil","Bulgaria":"Bulgarie","Cabo Verde":"Cap-Vert","Cambodia":"Cambodge",
-    "Cameroon":"Cameroun","Central African Rep.":"République centrafricaine",
-    "Chad":"Tchad","Chile":"Chili","China":"Chine","Colombia":"Colombie","Comoros":"Comores",
-    "Croatia":"Croatie","Cyprus":"Chypre","Czech Republic":"République tchèque",
-    "Dem. Rep. Congo":"République démocratique du Congo","Denmark":"Danemark","Dominica":"Dominique",
-    "Dominican Rep.":"République dominicaine","Ecuador":"Équateur","Egypt":"Égypte",
-    "El Salvador":"Salvador","Eq. Guinea":"Guinée équatoriale","Eritrea":"Érythrée",
-    "Estonia":"Estonie","Ethiopia":"Éthiopie","Fiji":"Fidji","Finland":"Finlande","Gambia":"Gambie","Georgia":"Géorgie",
-    "Germany":"Allemagne","Greece":"Grèce","Grenada":"Grenade",
-    "Guinea":"Guinée","Guinea-Bissau":"Guinée-Bissau","Haiti":"Haïti",
-    "Hungary":"Hongrie","Iceland":"Islande","India":"Inde",
-    "Indonesia":"Indonésie","Iraq":"Irak","Ireland":"Irlande","Israel":"Israël",
-    "Italy":"Italie","Jamaica":"Jamaïque","Japan":"Japon","Jordan":"Jordanie","Kuwait":"Koweït",
-    "Kyrgyzstan":"Kirghizistan","Latvia":"Lettonie","Lebanon":"Liban","Lesotho":"Lesotho",
-    "Liberia":"Libéria","Libya":"Libye","Lithuania":"Lituanie",
-    "Malaysia":"Malaisie","Malta":"Malte","Marshall Islands":"Îles Marshall",
-    "Mauritania":"Mauritanie","Mauritius":"Maurice","Mexico":"Mexique","Micronesia":"Micronésie",
-    "Moldova":"Moldavie","Mongolia":"Mongolie","Montenegro":"Monténégro",
-    "Morocco":"Maroc","Myanmar":"Birmanie (Myanmar)","Namibia":"Namibie",
-    "Nauru":"Nauru","Nepal":"Népal","Netherlands":"Pays-Bas","New Zealand":"Nouvelle-Zélande",
-    "Nicaragua":"Nicaragua","Niger":"Niger","Nigeria":"Nigéria","North Korea":"Corée du Nord",
-    "North Macedonia":"Macédoine du Nord","Norway":"Norvège","Oman":"Oman","Pakistan":"Pakistan",
-    "Palau":"Palaos","Panama":"Panama","Papua New Guinea":"Papouasie-Nouvelle-Guinée", 
-    "New Caledonia":"Nouvelle-Calédonie", "Faeroe Is.":"Îles Féroé", "Pitcairn Is.":"Îles Pitcairn",
-    "Falkland Is.": "Îles Malouines/ Falkland","Greenland":"Groenland (Danemark)", "Fr. Polynesia":"Polynésie française",
-    "S. Geo. and the Is.":"Géorgie du Sud et les îles Sandwich du Sud", "Fr. S. Antarctic Lands":"Terres australes et antarctiques françaises",
-    "Heard I. and McDonald Is.":"Îles Heard et McDonald", "Br. Indian Ocean Ter.":"Territoires britanniques de l'Océan Indien",
-    "Cayman Is.":"Îles Caïmans", "Turks and Caicos Is.":"Îles Turques-et-Caïques", "Puerto Rico":"Porto Rico (États-Unis)",
-    "British Virgin Is.":"Îles Vierges britanniques"," U.S. Virgin Is.":"Îles Vierges des États-Unis",
-    "Paraguay":"Paraguay","Peru":"Pérou","Philippines":"Philippines","Poland":"Pologne",
-    "Portugal":"Portugal","Qatar":"Qatar","Romania":"Roumanie","Russia":"Russie","Rwanda":"Rwanda", 
-    "Saint Kitts and Nevis":"Saint-Christophe-et-Niévès","Saint Lucia":"Sainte-Lucie",
-    "Saint Vincent and the Grenadines":"Saint-Vincent-et-les-Grenadines",
-    "San Marino":"Saint-Marin","São Tome and Principe":"São Tomé-et-Principe",
-    "Saudi Arabia":"Arabie saoudite","Senegal":"Sénégal","Serbia":"Serbie","Seychelles":"Seychelles",
-    "Sierra Leone":"Sierra Leone","Singapore":"Singapour","Slovakia":"Slovaquie","Slovenia":"Slovénie",
-    "Solomon Is.":"Îles Salomon","Somalia":"Somalie","South Africa":"Afrique du Sud",
-    "South Korea":"Corée du Sud","S. Sudan":"Soudan du Sud","Spain":"Espagne",
-    "Sudan":"Soudan","Sweden":"Suède","Switzerland":"Suisse",
-    "Syria":"Syrie","Taiwan":"Taïwan","Tajikistan":"Tadjikistan","Tanzania":"Tanzanie",
-    "Thailand":"Thaïlande","Timor-Leste":"Timor oriental",
-    "Trinidad and Tobago":"Trinité-et-Tobago","Tunisia":"Tunisie","Turkey":"Turquie",
-    "Turkmenistan":"Turkménistan","Uganda":"Ouganda",
-    "United Arab Emirates":"Émirats arabes unis","United Kingdom":"Royaume-Uni",
-    "United States of America":"États-Unis","Uruguay":"Uruguay","Uzbekistan":"Ouzbékistan",
-    "Vatican City":"Vatican","Vietnam":"Viêt Nam","Yemen":"Yémen",
-    "Zambia":"Zambie",
+    "Albania": "Albanie", "Algeria": "Algérie", "Andorra": "Andorre",
+    "Antigua and Barbuda": "Antigua-et-Barbuda", "Argentina": "Argentine",
+    "Armenia": "Arménie", "Australia": "Australie", "Austria": "Autriche",
+    "Azerbaijan": "Azerbaïdjan", "Bahrain": "Bahreïn", "Barbados": "Barbade",
+    "Belarus": "Biélorussie", "Belgium": "Belgique", "Benin": "Bénin",
+    "Bhutan": "Bhoutan", "Bolivia": "Bolivie", "Bosnia and Herz.": "Bosnie-Herzégovine",
+    "Brazil": "Brésil", "Bulgaria": "Bulgarie", "Cabo Verde": "Cap-Vert", "Cambodia": "Cambodge",
+    "Cameroon": "Cameroun", "Central African Rep.": "République centrafricaine",
+    "Chad": "Tchad", "Chile": "Chili", "China": "Chine", "Colombia": "Colombie", "Comoros": "Comores",
+    "Croatia": "Croatie", "Cyprus": "Chypre", "Czech Republic": "République tchèque",
+    "Dem. Rep. Congo": "République démocratique du Congo", "Denmark": "Danemark", "Dominica": "Dominique",
+    "Dominican Rep.": "République dominicaine", "Ecuador": "Équateur", "Egypt": "Égypte",
+    "El Salvador": "Salvador", "Eq. Guinea": "Guinée équatoriale", "Eritrea": "Érythrée",
+    "Estonia": "Estonie", "Ethiopia": "Éthiopie", "Fiji": "Fidji", "Finland": "Finlande", "Gambia": "Gambie", "Georgia": "Géorgie",
+    "Germany": "Allemagne", "Greece": "Grèce", "Grenada": "Grenade",
+    "Guinea": "Guinée", "Guinea-Bissau": "Guinée-Bissau", "Haiti": "Haïti",
+    "Hungary": "Hongrie", "Iceland": "Islande", "India": "Inde",
+    "Indonesia": "Indonésie", "Iraq": "Irak", "Ireland": "Irlande", "Israel": "Israël",
+    "Italy": "Italie", "Jamaica": "Jamaïque", "Japan": "Japon", "Jordan": "Jordanie", "Kuwait": "Koweït",
+    "Kyrgyzstan": "Kirghizistan", "Latvia": "Lettonie", "Lebanon": "Liban", "Lesotho": "Lesotho",
+    "Liberia": "Libéria", "Libya": "Libye", "Lithuania": "Lituanie",
+    "Malaysia": "Malaisie", "Malta": "Malte", "Marshall Islands": "Îles Marshall",
+    "Mauritania": "Mauritanie", "Mauritius": "Maurice", "Mexico": "Mexique", "Micronesia": "Micronésie",
+    "Moldova": "Moldavie", "Mongolia": "Mongolie", "Montenegro": "Monténégro",
+    "Morocco": "Maroc", "Myanmar": "Birmanie (Myanmar)", "Namibia": "Namibie",
+    "Nauru": "Nauru", "Nepal": "Népal", "Netherlands": "Pays-Bas", "New Zealand": "Nouvelle-Zélande",
+    "Nicaragua": "Nicaragua", "Niger": "Niger", "Nigeria": "Nigéria", "North Korea": "Corée du Nord",
+    "North Macedonia": "Macédoine du Nord", "Norway": "Norvège", "Oman": "Oman", "Pakistan": "Pakistan",
+    "Palau": "Palaos", "Panama": "Panama", "Papua New Guinea": "Papouasie-Nouvelle-Guinée",
+    "New Caledonia": "Nouvelle-Calédonie", "Faeroe Is.": "Îles Féroé", "Pitcairn Is.": "Îles Pitcairn",
+    "Falkland Is.": "Îles Malouines/ Falkland", "Greenland": "Groenland (Danemark)", "Fr. Polynesia": "Polynésie française",
+    "S. Geo. and the Is.": "Géorgie du Sud et les îles Sandwich du Sud", "Fr. S. Antarctic Lands": "Terres australes et antarctiques françaises",
+    "Heard I. and McDonald Is.": "Îles Heard et McDonald", "Br. Indian Ocean Ter.": "Territoires britanniques de l'Océan Indien",
+    "Cayman Is.": "Îles Caïmans", "Turks and Caicos Is.": "Îles Turques-et-Caïques", "Puerto Rico": "Porto Rico (États-Unis)",
+    "British Virgin Is.": "Îles Vierges britanniques", " U.S. Virgin Is.": "Îles Vierges des États-Unis",
+    "Paraguay": "Paraguay", "Peru": "Pérou", "Philippines": "Philippines", "Poland": "Pologne",
+    "Portugal": "Portugal", "Qatar": "Qatar", "Romania": "Roumanie", "Russia": "Russie", "Rwanda": "Rwanda",
+    "Saint Kitts and Nevis": "Saint-Christophe-et-Niévès", "Saint Lucia": "Sainte-Lucie",
+    "Saint Vincent and the Grenadines": "Saint-Vincent-et-les-Grenadines",
+    "San Marino": "Saint-Marin", "São Tome and Principe": "São Tomé-et-Principe",
+    "Saudi Arabia": "Arabie saoudite", "Senegal": "Sénégal", "Serbia": "Serbie", "Seychelles": "Seychelles",
+    "Sierra Leone": "Sierra Leone", "Singapore": "Singapour", "Slovakia": "Slovaquie", "Slovenia": "Slovénie",
+    "Solomon Is.": "Îles Salomon", "Somalia": "Somalie", "South Africa": "Afrique du Sud",
+    "South Korea": "Corée du Sud", "S. Sudan": "Soudan du Sud", "Spain": "Espagne",
+    "Sudan": "Soudan", "Sweden": "Suède", "Switzerland": "Suisse",
+    "Syria": "Syrie", "Taiwan": "Taïwan", "Tajikistan": "Tadjikistan", "Tanzania": "Tanzanie",
+    "Thailand": "Thaïlande", "Timor-Leste": "Timor oriental",
+    "Trinidad and Tobago": "Trinité-et-Tobago", "Tunisia": "Tunisie", "Turkey": "Turquie",
+    "Turkmenistan": "Turkménistan", "Uganda": "Ouganda",
+    "United Arab Emirates": "Émirats arabes unis", "United Kingdom": "Royaume-Uni",
+    "United States of America": "États-Unis", "Uruguay": "Uruguay", "Uzbekistan": "Ouzbékistan",
+    "Vatican City": "Vatican", "Vietnam": "Viêt Nam", "Yemen": "Yémen",
+    "Zambia": "Zambie",
 };
 
 function getCountryNameFr(nameEn) {
@@ -106,12 +106,12 @@ function bestMatch(countryName, candidates) {
             best = c;
         }
     }
-    const threshold = Math.floor(normA.length * 0.5);  
+    const threshold = Math.floor(normA.length * 0.5);
     return bestDist <= threshold ? best : null;
 }
 
 let worldData, gunData, gdpData;
-let gunsField = "Estimation de possession d'armes civiles pour 100 personnes";
+let gunsField = "Estimation des possession d'armes civiles pour 100 personnes";
 let indicator = "guns";
 let gdpYear = "2023";
 
@@ -130,12 +130,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 /* 3. INFOBOX*/
 const info = L.control();
-info.onAdd = function() {
+info.onAdd = function () {
     this._div = L.DomUtil.create('div', 'info');
     this.update();
     return this._div;
 };
-info.update = function(props) {
+info.update = function (props) {
     if (!props) {
         this._div.innerHTML = "Survolez un pays";
         return;
@@ -159,34 +159,37 @@ info.addTo(map);
 ============================================================ */
 const getColorGuns = d =>
     d > 100 ? '#800026' :
-    d > 50 ? '#BD0026' :
-    d > 20 ? '#E31A1C' :
-    d > 10 ? '#FC4E2A' :
-    d > 5 ? '#FD8D3C' :
-    d > 1 ? '#FEB24C' : '#FFEDA0';
+        d > 50 ? '#BD0026' :
+            d > 20 ? '#E31A1C' :
+                d > 10 ? '#FC4E2A' :
+                    d > 5 ? '#FD8D3C' :
+                        d > 1 ? '#FEB24C' : '#FFEDA0';
 
 const getColorGDP = d =>
     d > 1e12 ? '#084594' :
-    d > 5e11 ? '#2171b5' :
-    d > 1e11 ? '#4292c6' :
-    d > 5e10 ? '#6baed6' :
-    d > 1e10 ? '#9ecae1' :
-    d > 1e9 ? '#c6dbef' : '#deebf7';
+        d > 5e11 ? '#2171b5' :
+            d > 1e11 ? '#4292c6' :
+                d > 5e10 ? '#6baed6' :
+                    d > 1e10 ? '#9ecae1' :
+                        d > 1e9 ? '#c6dbef' : '#deebf7';
 
 const style = feature => {
     const val = indicator === "guns"
         ? feature.properties[gunsField]
         : feature.properties[gdpYear];
 
-    return {
-        fillColor: indicator === "guns" ? getColorGuns(val) : getColorGDP(val),
-        color: "#222",
-        weight: 1,
-        fillOpacity: 0.7
-    };
+     // Vérifier si la valeur est dans les seuils actifs
+     const thresholds = indicator === "guns" ? gunThresholds : gdpThresholds;
+     const isVisible = thresholds.some(t => t.active && val >= t.min && val < t.max);
+     
+     return {
+          fillColor: indicator === "guns" ? getColorGuns(val) : getColorGDP(val),
+          color: "#222",
+          weight: 1,
+          fillOpacity: isVisible ? 0.7 : 0
+     };
 };
 
-/* 6. INTERACTIONS SANS BUG (pas de déplacement) */
 const hoverStyle = {
     weight: 2,
     color: "#000"
@@ -195,12 +198,8 @@ const hoverStyle = {
 function highlightFeature(e) {
     const layer = e.target;
     const props = layer.feature.properties;
-
-    // Ajoute le nom français dans les propriétés
     props.name_fr = getCountryNameFr(props.name);
-
     info.update(props);
-
     layer.setStyle({
         weight: 2,
         color: '#000',
@@ -209,7 +208,6 @@ function highlightFeature(e) {
 
     layer.bringToFront();
 }
-
 
 function resetHighlight(e) {
     geoLayer.resetStyle(e.target);
@@ -229,28 +227,63 @@ function onEachFeature(feature, layer) {
 }
 
 /* 7. LÉGENDE */
+let gunThresholds = [
+    { min: 1, max: 5, active: true },
+    { min: 5, max: 10, active: true },
+    { min: 10, max: 20, active: true },
+    { min: 20, max: 50, active: true },
+    { min: 50, max: 100, active: true },
+    { min: 100, max: Infinity, active: true }
+];
+
+let gdpThresholds = [
+    { min: 0, max: 1e9, active: true },
+    { min: 1e9, max: 1e10, active: true },
+    { min: 1e10, max: 5e10, active: true },
+    { min: 5e10, max: 1e11, active: true },
+    { min: 1e11, max: 5e11, active: true },
+    { min: 5e11, max: 1e12, active: true },
+    { min: 1e12, max: Infinity, active: true }
+];
+
 const legend = L.control({ position: "bottomright" });
 function updateLegend() {
     map.removeControl(legend);
-    legend.onAdd = function() {
-        const div = L.DomUtil.create("div", "legend");
-        let grades, getColor;
+    legend.onAdd = function () {
+       const div = L.DomUtil.create("div", "legend");
+       const thresholds = indicator === "guns" ? gunThresholds : gdpThresholds;
+       const getColor = indicator === "guns" ? getColorGuns : getColorGDP;
 
-        if (indicator === "guns") {
-            grades = [0, 1, 5, 10, 20, 50, 100];
-            getColor = getColorGuns;
-        } else {
-            grades = [0, 1e9, 1e10, 5e10, 1e11, 5e11, 1e12];
-            getColor = getColorGDP;
-        }
+       thresholds.forEach(t => {
+           const label = `${t.min.toLocaleString()} – ${t.max === Infinity ? '+' : t.max.toLocaleString()}`;
+           
+           const row = document.createElement("div");
+           row.style.display = "flex";
+           row.style.alignItems = "center";
+           row.style.marginBottom = "5px";
+           row.style.cursor = "pointer";
 
-        grades.forEach((g, i) => {
-            div.innerHTML +=
-                `<i style="background:${getColor(g+0.1)}"></i>
-                 ${g.toLocaleString()} ${grades[i+1] ? "– " + grades[i+1].toLocaleString() + "<br>" : "+"}`;
-        });
+           const colorBox = document.createElement("span");
+           colorBox.style.display = "inline-block";
+           colorBox.style.width = "18px";
+           colorBox.style.height = "18px";
+           colorBox.style.backgroundColor = getColor(t.min + 0.1);
+           colorBox.style.marginRight = "8px";
+           colorBox.style.border = t.active ? "2px solid #000" : "2px solid #ccc";
 
-        return div;
+           const toggleState = () => {
+              t.active = !t.active;
+              colorBox.style.border = t.active ? "2px solid #000" : "2px solid #ccc";
+              geoLayer.setStyle(style);
+           };
+
+           row.appendChild(colorBox);
+           row.appendChild(document.createTextNode(label));
+           row.addEventListener("click", toggleState);
+           div.appendChild(row);
+       });
+
+      return div;
     };
     legend.addTo(map);
 }
